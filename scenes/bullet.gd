8b474519842519196c71b6a43126e95aefb90e1c
Palmério <palmerio_julio@hotmail.com>
2021-09-26 11:06:48 -0300

    Mudando movimento do tank e alterando a rotação do canhão

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,17 +1,28 @@
 extends Area2D
 
-# variável para controle de velocidade da bala.
-var vel = 250 
+const MAX_DIST = 250
+var vel = 400 
 
 # variável do angulo em 90 graus. Usando seget para chamar a função setar direção. 
 var dir = Vector2(0,-1) setget setar_direcao
+
+# variável para pegar a posição inicial da bala
+onready var init_position = global_position
+
+# Criando variável para saber se um objeto está vivo.
+var live = true
 
 func _ready():
 	pass 
 
 # Chamado todos os quadros. 'delta' é o tempo decorrido desde o quadro anterior.
 func _process(delta):
-	translate(dir * vel * delta)
+	# Se a variável live estiver true.
+	if (live):
+		# se a distância da posiçao da bala para o tank for maior ou iqual a distância máximo
+		if (global_position.distance_to(init_position) >= MAX_DIST):
+			autodestroy() #Função para destruir a bala.
+		translate(dir * vel * delta)
 	
 # Função criada pra saber quando qualquer instância do objeto bullet saiu da tela
 func _on_notifier_screen_exited():
@@ -23,3 +34,25 @@ func setar_direcao(val):
 	dir = val
 	# A rotação é o angulo da direção
 	rotation = dir.angle()
+
+	# Método para saber se a bala está dentro de outro 
+	# corpo físico(só executa quando bate na parede ou enimigos). 
+func _on_bullet_body_entered(body):
+	# saber o a colisão aconteceu com o leyer 4.
+	if (body.collision_layer == 4):
+		autodestroy() #Função para destruir a bala.
+		
+func autodestroy():
+	$smoke.emitting = false # Parando a particula da fumaça.
+	live = false 			# Setando false a variável live.
+	$sprite.visible = false # Deixando o sprite da bala invisivel .
+	$ani_destruction.play("explode") # Iniciando a animação da explosão.
+	# Desabilitando as colisões
+	# chamando a função call_deferred para que o monitoramento seja 
+	# parado no final da função principal.
+	call_deferred("set_monitoring", false)
+	call_deferred("set_monitoring", false)
+	# Dando preferência de execução até finalizar a animação de explosão.
+	yield($ani_destruction, "animation_finished")
+	# Liberando memoria(apaga o objeto).
+	queue_free() 
--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -1,17 +1,34 @@
+# visualizando alterações em modo de edição.
+tool
+
+# incorporando o objeto KinematicBody2D.
 extends KinematicBody2D
 
+""" variável para armazena o id do objeto tank e criar um grupo.
+onready só inicia a variável quando todo o script estiver pronto."""
+onready var bullet_tank_group = "bullet-" + str(self)
+
+""" Criando uma constante para criar a rotação de 90 graus por segundo usando 
+PI(180 graus) Pi dividido por 2, 90 graus - Pi multiplicado por 2 360 graus
+OBS: 1 PI é meia esfera."""
+const ROT_VEL = PI / 2
+
 # variável para o controle de velocidade.
-var speed = 100
-# variável para fazer um pré carregamento do objeto bullet.
+const MAX_SPEED = 100
+
+# pré carregamento do objeto bullet.
 var pre_bullet = preload("res://scenes/bullet.tscn")
 
-""" Um dos benefícios fundamentais da exportação de variáveis é tê-las visíveis e editáveis 
-​​no editor. Dessa forma, artistas e designers de jogos podem modificar valores que mais tarde 
-influenciam a execução do programa. """
+# Variável para celeração
+var aceleracao = 0
+
+""" Um dos benefícios fundamentais da exportação de variáveis é tê-las 
+visíveis e editáveis ​​no editor. Dessa forma, artistas e designers de jogos 
+podem modificar valores que mais tarde influenciam a execução do programa. """
 
 # Expotando variáveis de membros, para telas visíveis no editor de variáveis do script.
-export(int, "bigRed", "blue", "dark", "darkLarge", "green", "huge", "red", "sand") var bodie = 0
-export(int, "red1", "blue", "dark1", "dark2", "green", "sand1", "red2", "sand2") var barrel = 0
+export(int, "bigRed", "blue", "dark", "darkLarge", "green", "huge", "red", "sand") var bodie = 0 setget set_bodie
+export(int, "red1", "blue", "dark1", "dark2", "green", "sand1", "red2", "sand2") var barrel = 0 setget set_barrel
 
 # Criando lista para novas texturas do tanque
 var bodies = [
@@ -38,57 +55,111 @@ var barrels = [
 ]
 
 func _ready():
+	pass
+
+# Função para redesenhar o objetos.
+func _draw():
 	$Sprite.texture = load(bodies[bodie])
 	$barrel/sprite.texture = load(barrels[barrel])
 	
-
 # Chamado todos os quadros. 'delta' é o tempo decorrido desde o quadro anterior.
-func _process(delta):
+func _physics_process(delta):
+	# Se estiver no modo editor saia da função process.
+	if (Engine.editor_hint):
+		return
 	
-	var dir_x = 0 
-	var dir_y = 0 
-	
-	#Entradas dos comandos.
-	if Input.is_action_pressed("ui_right"):
-		dir_x += 1
-		
-	if Input.is_action_pressed("ui_left"):
-		dir_x -= 1
-	
-	if Input.is_action_pressed("ui_up"):
-		dir_y -= 1
-		
-	if Input.is_action_pressed("ui_down"):
-		dir_y += 1
-	
+#	var dir_x = 0 
+#	var dir_y = 0 
+#
+#	#Entradas dos comandos.
+#	if (Input.is_action_pressed("ui_right")):
+#		dir_x += 1
+#
+#	if (Input.is_action_pressed("ui_left")):
+#		dir_x -= 1
+#
+#	if (Input.is_action_pressed("ui_up")):
+#		dir_y -= 1
+#
+#	if (Input.is_action_pressed("ui_down")):
+#		dir_y += 1
+#
 	# Input do botões do tiro
-	if Input.is_action_just_pressed("ui_shoot"):
-		
+	if (Input.is_action_just_pressed("ui_shoot")):
+
 		# Limitando quantidade de tiros. 
 		# Se a quantidade de nós do grupo cannon_bullets for menor que 6 atire.		
-		if get_tree().get_nodes_in_group("cannon_bullets").size() < 6:
-			
+		if (get_tree().get_nodes_in_group(bullet_tank_group).size() < 6):
+
 			# Crirando uma instância(cópia) do objeto bullet.
 			var bullet = pre_bullet.instance()
-			
+
 			# Pegando a posição global do objeto "muzzel" e atribuiando na posição global 
 			# da variável bullet. 
 			bullet.global_position = $barrel/muzzle.global_position
-			
+
 			# Atribuindo o valor do seno e coseno na variável direção ou seja o x e o y do 
 			# mouse. Usando o normalized para não calcular a velocidade. 
-			bullet.dir = Vector2( cos(rotation), sin(rotation) ).normalized()
-			
+			bullet.dir = Vector2( cos($barrel.global_rotation), sin($barrel.global_rotation) ).normalized()
+
+			# Adicionando um grupo em tempo de execução para limitar a quantidade de tiros de cada tank. 
+			bullet.add_to_group(bullet_tank_group)
+
 			# Chamando a animação fire do disparo
-			$barrel/animation.play("fire")
-			
+			$barrel/anim.play("fire")
+
 			# Colocando o objeto bullet na cena principail.
 			get_parent().add_child(bullet)
+#
+#	# Colando o tanque pra seguir a direção do mouse
+#	look_at(get_global_mouse_position())
+#
+#	# Chamando o método move_and_slide para movimentar o objeto apartir do ponto original 
+#	# e colidir com paredes, movimentando para direção x ou y a cada pixel.
+#	move_and_slide(Vector2(dir_x , dir_y) * speed)
 	
-	# Colando o tanque pra seguir a direção do mouse
-	look_at(get_global_mouse_position())
+	var rotacao = 0 
+	var direcao = 0 
 	
-	# Chamando função translate para movimentar o objeto apartir do ponto original, 
-	# movimentando para direção x ou y a cada pixel.
-	translate( Vector2(dir_x , dir_y) * delta * speed)
-	pass
+	# Controle da rotação
+	if (Input.is_action_pressed("ui_right")):
+		rotacao += 1
+	if (Input.is_action_pressed("ui_left")):
+		rotacao -= 1
+	# Controle da direção
+	if (Input.is_action_pressed("ui_up")):
+		direcao += 1
+	if (Input.is_action_pressed("ui_down")):
+		direcao -= 1
+	# Rotacionando o tank
+	rotate(ROT_VEL * rotacao * delta) 
+	
+	# Se diração for diferente de 0
+	#if (direcao != 0):
+		# Metódo lerp interpola(interompe) linearmente entre dois valores por um valor como normalizador.
+		# Vai de 0 a 100 com uma variação(peso) de 1 decimo.
+	aceleracao = lerp(aceleracao , MAX_SPEED * direcao , .03)
+	#else:
+		#aceleracao = lerp(aceleracao, 0, .05)
+	
+	print(aceleracao)
+	
+	# Chamando o método move_and_slide para movimentar o objeto apartir do ponto original 
+	#  e colidir com paredes, movimentando para direção x ou y a cada pixel.
+	move_and_slide(Vector2( cos(rotation), sin(rotation) ) * aceleracao)
+	
+	# Apontando o barrel para a possição atual do mouse.
+	$barrel.look_at(get_global_mouse_position())
+	
+# Função para alterar o corpo de tank.
+func set_bodie(val):
+	bodie = val
+	if (Engine.editor_hint):# Se a godot estiver no modo editor.
+		update()# Execute a função Draw.
+		
+# Função para alterar o corpo de tank.
+func set_barrel(val):
+	barrel = val
+	if (Engine.editor_hint): # Se a godot estiver no modo editor.
+		update() # Execute a função Draw.
+	
